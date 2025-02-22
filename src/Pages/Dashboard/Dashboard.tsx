5cde2532f4e9f67fe6f167d1b93410de85a74afa
Gal Cohen <galcohen211@gmail.com>
2025-02-22 11:21:03 +0200

    feat: main page design (gym owner)

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Dashboard.less';
+import logo from '../../assets/Logo/shape-up.png'
 interface DashboardProps {
   children?: React.ReactNode;
 }
@@ -7,8 +8,7 @@ const Dashboard: React.FC<DashboardProps> = () => {
   return (
     <div className="container">
       <aside className="sidebar">
-        <div className="logo" />
-        <nav className="menuItem" />
+        <img src={logo} className="logo" />
       </aside>
       <main className="main">
         <div className="content">
