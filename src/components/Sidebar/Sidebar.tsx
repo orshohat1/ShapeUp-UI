9be824bdc9ea76065cf9021c8a399668908f77b3
Gal Cohen <galcohen211@gmail.com>
2025-03-07 14:52:42 +0200

    Fixed spinner

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import "./Sidebar.less";
 import logo from "../../assets/Logo/shape-up.png";
-import { HomeOutlined, EditOutlined, LogoutOutlined, DotChartOutlined } from "@ant-design/icons";
+import {
+  HomeOutlined,
+  EditOutlined,
+  LogoutOutlined,
+  DotChartOutlined,
+} from "@ant-design/icons";
 import { useUserProfile } from "../../context/useUserProfile";
 import { useNavigate } from "react-router-dom";
 
 const Sidebar: React.FC<{ user?: any }> = () => {
   const { userProfile } = useUserProfile();
   const navigate = useNavigate();
+
+  const handleEdit = () => {
+    console.log("Edit clicked!");
+    // Add edit functionality
+  };
+
+  const handleLogout = () => {
+    console.log("Logout clicked!");
+    // Add logout functionality
+  };
 
   return (
     <aside className="col-auto align-items-center justify-content-center sidebar">
@@ -45,35 +60,16 @@ const Sidebar: React.FC<{ user?: any }> = () => {
             {userProfile?.firstName} {userProfile?.lastName}
           </span>
           <div className="user-actions-container">
-            <EditOutlined className="edit-icon" />
-            <LogoutOutlined className="logout-icon" />
+            <button className="icon-button" onClick={handleEdit}>
+              <EditOutlined className="edit-icon" />
+            </button>
+            <button className="icon-button" onClick={handleLogout}>
+              <LogoutOutlined className="logout-icon" />
+            </button>
           </div>
         </div>
       </div>
     </aside>
-
-    // <aside className="sidebar">
-    //   {/* ShapeUp Logo */}
-    //   <div className="logo-container">
-    //     <img src={logo} alt="ShapeUp Logo" className="logo" />
-    //   </div>
-
-    //   {/* Navigation */}
-    //   <div className="menu">
-    //     <div className="menu-item active">
-    //       <HomeOutlined style={{ marginRight: "8px" }} />
-    //       Gyms
-    //     </div>
-    //   </div>
-
-    //   {/* User Profile */}
-    //   {user && (
-    //     <div className="user-info">
-    //       <img src={user.avatarUrl} alt="User Avatar" className="user-avatar" />
-    //       <span className="user-name">{user.firstName} {user.lastName}</span>
-    //     </div>
-    //   )}
-    // </aside>
   );
 };
 
